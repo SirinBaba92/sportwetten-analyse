7974caac332b11c934a86b715312f1fafe0f89e6
SirinBaba92 <altuerk.baran@outlook.com>
2026-02-16 11:41:03 +0100

    fixx

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -42,10 +42,8 @@ error_handler
 
 # Logging konfigurieren
 
-logging.basicConfig(
-format=’%(asctime)s - %(name)s - %(levelname)s - %(message)s’,
-level=logging.INFO
-)
+logging.basicConfig(format=’%(asctime)s - %(name)s - %(levelname)s - %(message)s’,
+level=logging.INFO)
 logger = logging.getLogger(**name**)
 
 def main():
